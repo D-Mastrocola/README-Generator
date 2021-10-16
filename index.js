7e5dff38fb3d24652723deca4c71046f2edd37bf
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-10-15 20:34:30 -0400

    inital input

---
+++ index.js
@@ -0,0 +1,104 @@
+//const { writeFile } = require('./utils/generate-site.js');
+const inquirer = require("inquirer");
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title for your project?",
+      validate: (titleInput) => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log("Please enter your project title!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Provide a description of the project (Required)",
+    },
+    {
+      type: "confirm",
+      name: "confirmInstallInstructions",
+      message:
+        'Would you like to enter some installation instructions?',
+      default: true,
+    },
+    {
+      type: "input",
+      name: "installInstructions",
+      message: "Provide some information about the installation:",
+      when: ({ confirmInstallInstructions }) => {
+        if (confirmInstallInstructions) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+    },
+    {
+      type: "confirm",
+      name: "confirmUsageInstructions",
+      message:
+        'Would you like to enter some usage instructions?',
+      default: true,
+    },
+    {
+      type: "input",
+      name: "usageInstructions",
+      message: "Provide some information about the usage:",
+      when: ({ confirmUsageInstructions }) => {
+        if (confirmUsageInstructions) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+    },
+    {
+      type: "confirm",
+      name: "confirmContributionGuidelines",
+      message:
+        'Would you like to enter any contribution guidelines?',
+      default: true,
+    },
+    {
+      type: "input",
+      name: "contributionGuidelines",
+      message: "Provide some information about the usage:",
+      when: ({ confirmContributionGuidelines }) => {
+        if (confirmContributionGuidelines) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+    },
+    {
+      type: "confirm",
+      name: "confirmTestInstructions",
+      message:
+        'Would you like to enter any contribution guidelines?',
+      default: true,
+    },
+    {
+      type: "input",
+      name: "testInstructions",
+      message: "Provide some testing instructions:",
+      when: ({ confirmTestInstructions }) => {
+        if (confirmTestInstructions) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+    },
+  ]);
+};
+promptUser().then((data) => {
+  console.log(data)
+});
